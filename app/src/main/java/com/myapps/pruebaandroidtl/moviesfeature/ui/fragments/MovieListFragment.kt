efb6037740d135601c1d397ce62e9d218bc9a069
Jefferson Roman C <97921301+jeff1927@users.noreply.github.com>
2022-10-30 15:53:46 -0500

    Develop change remote mediator for network bound resource (#4)
    * network bound resource inline function has been added
    
    * missing staged file
    
    * add dependencies for unit testing

--- app/src/main/java/com/myapps/pruebaandroidtl/moviesfeature/ui/fragments/MovieListFragment.kt
+++ app/src/main/java/com/myapps/pruebaandroidtl/moviesfeature/ui/fragments/MovieListFragment.kt
@@ -40,29 +40,24 @@ class MovieListFragment : Fragment() {
     }
 
     private fun setupRecyclerView() {
-        binding.rvMovies.apply {
+        binding.rvMovies.apply { 
             layoutManager = LinearLayoutManager(requireContext())
             adapter = MoviesAdapter(setOnItemClicked = {
-                findNavController().navigate(
-                    MovieListFragmentDirections.actionMovieListFragmentToMovieDetailFragment(
-                        it
-                    )
-                )
+                findNavController().navigate(MovieListFragmentDirections.actionMovieListFragmentToMovieDetailFragment(it))
             })
         }
     }
-
     private fun setupObserver() {
         viewModel.moviesList.observe(viewLifecycleOwner) { response ->
             when (response) {
-                is Resource.Success -> {
+                is Resource.Success-> {
                     binding.rvMovies.adapter?.let {
                         response.data?.let { moviesList ->
                             (it as MoviesAdapter).submitList(moviesList)
                         }
                     }
                 }
-                is Resource.Error -> {
+                is Resource.Error-> {
                     response.error?.let { message ->
                         Toast.makeText(
                             requireContext(),
